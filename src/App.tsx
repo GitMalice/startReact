2a070e84a957a0863d0a620b804a38efa5c723fb
Nelly Heng <nellyheng@gmail.com>
2024-08-04 12:11:13 +0200

    set the base of the NavBar component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import Heading from "./components/Heading";
 import { Section } from "./components/Section";
 import NavBar from "./components/NavBar";
 import List from "./components/List";
+import "bootstrap/dist/css/bootstrap.css";
+import imagePath from "./assets/tux_logo.png";
 
 function App() {
   return (
     <>
-      <NavBar />
+      <NavBar brandName="Go Linux PROJECT" imageSrcPath={imagePath} />
       <Heading title={"Hello"} />
       <Section title={"This is me"}>This is my section</Section>
       <List
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,34 @@
-import React from "react";
+interface NavBarProps {
+  brandName: string;
+  imageSrcPath: string;
+}
 
-function NavBar() {
+function NavBar({ brandName, imageSrcPath }: NavBarProps) {
   return (
-    <nav className="navbar navbar-light bg-light">
-      <a className="navbar-brand" href="#">
-        <img
-          src="/docs/4.0/assets/brand/bootstrap-solid.svg"
-          width="30"
-          height="30"
-          className="d-inline-block align-top"
-          alt=""
-        />
-        Go Linux PROJECT
-      </a>
+    <nav className="navbar navbar-dark bg-dark shadow">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          <img
+            src={imageSrcPath}
+            width="70"
+            height="70"
+            className="d-inline-block align-center"
+            alt=""
+          />
+          <span className="fw-bolder fs-4 p-3">{brandName}</span>
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+      </div>
     </nav>
   );
 }
