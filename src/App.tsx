a4911de3dba0684c9f327062ea1613cabc9d1a85
Nelly Heng <nellyheng@gmail.com>
2024-08-05 11:46:07 +0200

    set the navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,19 @@ import { Section } from "./components/Section";
 import NavBar from "./components/NavBar";
 import List from "./components/List";
 import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import imagePath from "./assets/tux_logo.png";
+import "./App.css";
 
 function App() {
+  let items = ["Home", "Tuto1", "Tuto2"];
   return (
     <>
-      <NavBar brandName="Go Linux PROJECT" imageSrcPath={imagePath} />
+      <NavBar
+        brandName="Go Linux PROJECT"
+        imageSrcPath={imagePath}
+        navItems={items}
+      />
       <Heading title={"Hello"} />
       <Section title={"This is me"}>This is my section</Section>
       <List
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,23 @@
+import { useState } from "react";
+import "../App.css";
+
 interface NavBarProps {
   brandName: string;
   imageSrcPath: string;
+  navItems: string[];
 }
 
-function NavBar({ brandName, imageSrcPath }: NavBarProps) {
+function NavBar({ brandName, imageSrcPath, navItems }: NavBarProps) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
   return (
-    <nav className="navbar navbar-dark bg-dark shadow">
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark shadow">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
           <img
             src={imageSrcPath}
-            width="70"
-            height="70"
+            width="60"
+            height="60"
             className="d-inline-block align-center"
             alt=""
           />
@@ -20,14 +26,51 @@ function NavBar({ brandName, imageSrcPath }: NavBarProps) {
         <button
           className="navbar-toggler"
           type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon" />
         </button>
+        <div
+          className="collapse
+         navbar-collapse"
+          id="navbarSupportedContent"
+        >
+          <ul className="navbar-nav me-auto mb-2 mb-md-1">
+            {navItems.map((items, index) => (
+              <li
+                key={items}
+                className="nav-item"
+                onClick={() => setSelectedIndex(index)}
+              >
+                <a
+                  className={
+                    selectedIndex == index
+                      ? "nav-link active fw-bold"
+                      : "nav-link"
+                  }
+                  href="#"
+                >
+                  {items}
+                </a>
+              </li>
+            ))}
+          </ul>
+          <form className="d-flex me-3">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
       </div>
     </nav>
   );
